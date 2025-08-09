9d9164b55540b315d1ee21c196a40c29b55738a0
shamun-khatri <shamunkhatri@gmail.com>
2025-08-09 17:54:56 +0530

    implemented  tcp listner

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use std::net::TcpListener;
 fn main() {
     println!("Logs from your program will appear here!");
 
+    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+
     for stream in listener.incoming() {
         match stream {
             Ok(_stream) => {

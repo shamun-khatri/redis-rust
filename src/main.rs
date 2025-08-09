7277d89568f97ec9854f6fc47b85152a9091be96
shamun khatri <shamunkhatri@gmail.com>
2025-08-09 21:38:09 +0530

    running a loop to reponce for every PING

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
-#![allow(unused_imports)]
-use std::io::Write;
-use std::net::TcpListener;
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 fn main() {
-    println!("Logs from your program will appear here!");
-
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                stream.write_all(b"+PONG\r\n").unwrap();
+                println!("accepted new connection");
+
+                let mut buf = [0; 512];
+                loop {
+                    let read_count = stream.read(&mut buf).unwrap();
+                    if read_count == 0 {
+                        break;
+                    }
+
+                    stream.write(b"+PONG\r\n").unwrap();
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
